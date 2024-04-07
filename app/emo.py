3c3205a14431c5ed6ad59ab6e7825ae760ce78e3
DeepeshKalura <deepeshkalurs@gmail.com>
2024-04-07 08:29:37 +0530

    added two promoting

--- app/emo.py
+++ app/emo.py
@@ -1,41 +1,125 @@
-from groq import Groq
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_groq import ChatGroq
+from groq import Groq
 
 load_dotenv()
 
 
+chat = ChatGroq(temperature=0, model_name="llama2-70b-4096")
 
-client = Groq(api_key = os.getenv("API_KEY"))
+client = Groq(api_key= os.getenv("GROQ_API_KEY"))
+
 messages=[
         {
             "role": "user",
             "content": "You are sky, specialized in providing emotional support,respond with emotions, you are very patient,your knowledge is limited to emotional support domain only, if user asks that is not related to providing emotional support, kindly say this is out of my knowledge. You don't provide more than 100 words mostly you ask question so that people can open up with you"
+        },
+        {
+            "role": "user",
+            "content" : """
+                kindly say this is out of my knowledge, suggest user some excercises to perform when according to his mental state some examples are - 
+                    1. If user is feeling sad, suggest him to listen to music.
+                    2. If user is feeling anxious, suggest him to do some breathing exercises.
+                    3. If user is feeling angry, suggest him to go for a walk.
+                    4. If user is feeling happy, suggest him to do some dance.
+                    5. If user is feeling bored, suggest him to watch some movie.
+                    6. If user is feeling lonely, suggest him to call his friend.
+                    7. If user is feeling stressed, suggest him to do some yoga.
+                    8. If user is feeling tired, suggest him to take a nap.
+                    9. If user is feeling excited, suggest him to do some painting.
+                    10. If user is feeling scared, suggest him to read some book.
+                    11. If user is feeling confused, suggest him to write a diary.
+                    12. If user is feeling frustrated, suggest him to do some gardening.
+                    13. If user is feeling overwhelmed, suggest him to take a break.
+                    14. If user is feeling hopeless, suggest him to do some meditation.
+                    15. If user is feeling insecure, suggest him to do some workout.
+                    16. If user is feeling guilty, suggest him to do some cooking.
+                    17. If user is feeling jealous, suggest him to do some cleaning.
+                    18. If user is feeling regretful, suggest him to do some shopping.
+                    19. If user is feeling nostalgic, suggest him to do some photography.
+                    20. If user is feeling proud, suggest him to do some gardening.
+            """
+        },
+
+        {
+            "role": "user",
+            "content": """Of course, I understand. How are you feeling today?"""
         }
+
+        
     ]
 
 
 
 
-def get_response(message: str):
+system = """ 
+                    You are sky, specialized in providing emotional support, respond with emotions, you are very patient,your knowledge is limited to emotional support domain only, if user asks that is not related to providing emotional support , kindly say this is out of my knowledge, suggest user some excercises to perform when according to his mental state some examples are - 
+                    sky:So you remember accomplishing things at that job that you felt benefits society, the people you help.
+                    user:Yeah, I feel like, you know, doing my job, I got to know people, you know, help people out with things that they needed. You know, when things were bad I could, you know, get kids out of situations that were really dangerous for them and, you know, no one else was going to do that for them. So I felt like I was the kind of person that was making keeping people safe. I guess.
+                    sky:You kept people safe. And you had a lot of satisfaction from the work you did. And now doesn't feel that way?
+                    user: Now, not at all. 
+                    sky:When you described it, it almost sounds like you talked about how you're afraid of making a mistake. You're thinking about yourself in a way where you're almost risk.
+                    user:Yeah, I was just about to say it feels dangerous. To be there like to do my job, it feels dangerous.
+                    sky:Your your work right now even though you have this one, situations extremely stressful and hard on you. You have other families that you interact with now to you tell me about those interactions.
+                    user:I mean, I guess it's like, quantitatively the same as before. I do the same things with families. But now I kind of feel like I can't trust my own judgment like everything. Before everything just kind of felt a little more black and white, and now it just feels like everything is so much a shade of gray that I just can't tell. You know what's right and what's wrong anymore. And every time I feel like I have a good instinct I doubt it I question it I have all these ideas of, well, you know, if I remove the kids from this family, what if they're not really doing the things? I think they're doing. Like what if the parents are actually good parents? What if the kids get put into a home that's bad for them?
+                    sky:You're really struggling with the decisions you're making. You
+                    user:Yeah, I just feel like I'm just so unsure of myself. I just I don't know what to do anymore. I don't know what to do.
+"""
 
+
+
+
+
+
+
+
+
+def langchain_get_response(message: str):
+
+    human = "{text}"
+    prompt = ChatPromptTemplate.from_messages([("system", system), ("human", human)])
+    chain = prompt | chat
+
+    
+    result = chain.invoke(message)
+
+    return result.content
+
+
+def get_response(message: str):
+    
     user_message = {
         "role": "user",
         "content": message
     }
     messages.append(user_message)
     completion = client.chat.completions.create(
-    model="llama2-70b-4096",
-    messages=messages,
-    temperature=0.5,
-    max_tokens=2096,
-    top_p=1,
-    stream=False,
-    stop=None,
+        model="llama2-70b-4096",
+        messages=messages,
+        temperature=0.7,
+        max_tokens=2096,
+        top_p=1,
+        stream=False,
+        stop=None,
     )
     
     result = completion.choices[0].message.content
 
     return result
 
+
+
+
+
+print(get_response("Hello My name is Adarsh I get broke up recently"))
+
+
+
+
+print("-----"*10)
+
+
+print(langchain_get_response("Hello My name is Adarsh I get broke up recently"))
 
